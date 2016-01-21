cb8aabc13406379e488cce4791b495d54e695a5d
red0509 <paa_pavlov@mail.ru>
2016-01-22 01:05:35 +0300

    1.05

--- ViewControllerPageView.m
+++ ViewControllerPageView.m
@@ -10,6 +10,10 @@
 
 @interface ViewControllerPageView ()
 
+@property (strong,nonatomic) NSMutableArray *timeArray;
+@property (strong,nonatomic) NSMutableArray *weekArray;
+@property (strong,nonatomic) NSMutableArray *subjectArray;
+
 @end
 
 @implementation ViewControllerPageView
@@ -17,6 +21,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+   
     self.pageTitles = @[@"Понедельник", @"Вторник", @"Среда", @"Четверг",@"Пятница",@"Суббота"];
         
     self.pageViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"PageViewController"];
@@ -32,6 +37,7 @@
     [self addChildViewController:self.pageViewController];
     [self.view addSubview:self.pageViewController.view];
     [self.pageViewController didMoveToParentViewController:self];
+    
     
      [self loadGroupReference:@"http://stud.sssu.ru/Rasp/Rasp.aspx?group=18856&sem=1"];
     
@@ -98,6 +104,9 @@
 }
 
 -(void) loadGroupReference:(NSString*) URLGroup{
+    self.timeArray = [NSMutableArray array];
+    self.weekArray = [NSMutableArray array];
+    self.subjectArray = [NSMutableArray array];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSURL *URL = [NSURL URLWithString:URLGroup];
         NSURLSession *session = [NSURLSession sharedSession];
@@ -110,16 +119,95 @@
               }
               HTMLDocument *home = [HTMLDocument documentWithData:data
                                                 contentTypeHeader:contentType];
+              NSInteger dayNum = 2;
+              NSNumber *dayRow;
+              // День недели
+              while (YES) {
+              HTMLElement *day = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(1)",(long)dayNum]];
+                  if ([day.textContent isEqualToString:@"Понедельник"]) {
+                      dayRow = day.attributes.allValues.lastObject;
+                      break;
+                  }else if (day.textContent == nil){
+                      break;
+                  }
+                  dayNum++;
+              }
+              NSInteger dayRowInt = [dayRow integerValue];
               
-             
+              NSInteger timeNum = dayNum;
+              NSInteger timeRow;
+              //Время
+              for (; timeNum < dayRowInt+dayNum; timeNum++) {
+                  
+                  if (timeNum == dayNum) {
+                      timeRow = 2;
+                  }else{
+                      timeRow = 1;
+                  }
+                  
+              HTMLElement *time = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)timeNum,(long)timeRow]];
+                  if ([time.textContent isEqualToString:@"2"]) {
+                      [self.timeArray addObject:[self.timeArray objectAtIndex:[self.timeArray count]-1]];
+                  }else{
+                      [self.timeArray addObject:time.textContent];
+
+                  }
+                }
+              NSInteger weekNum = dayNum;
+              NSInteger weekRow;
+              // номер Неделя
+              for (; weekNum < dayRowInt+dayNum; weekNum++) {
+                  
+                  if (weekNum == dayNum) {
+                      weekRow = 3;
+                  }else{
+                      weekRow = 2;
+                  }
+                  
+                  HTMLElement *week = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)weekNum,(long)weekRow]];
+                  if (![week.textContent isEqualToString:@"1"]) {
+                      [self.weekArray addObject:@"2"];
+                      [self.weekArray addObject:@"Привет"];
+                  }else{
+                      [self.weekArray addObject:week.textContent];
+                      
+                  }
+              }
               
+              NSInteger subjectNum = dayNum;
+              NSInteger subjectRow;
+              //Дисциплина
               
+              for (; subjectNum < dayRowInt+dayNum; subjectNum++) {
+                  
+                  NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"SELF contains[c] ст."];
+                  if (subjectNum == dayNum) {
+                      subjectRow = 4;
+                  }else{
+                      subjectRow = 3;
+                  }
+                  
+                  HTMLElement *subject = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)subjectNum,(long)subjectRow]];
+                  if ([subject.textContent isEqualToString:@"ст."]) {
+                      subjectRow--;
+                      HTMLElement *subject = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)subjectNum,(long)subjectRow]];
+                      [self.subjectArray addObject:subject.textContent];
+                      subjectRow++;
+                  }else{
+                      [self.subjectArray addObject:subject.textContent];
+
+                  }
+                  
+              }
+
               
-              HTMLElement *div = [home firstNodeMatchingSelector:@"#cmbDayOfWeek"];
-              
-            NSLog(@"div -  %@",div.textContent);
-              NSLog(@"div -  %@",div.attributes.allValues);
+           //              NSLog(@"div -  %@",day.attributes.allValues);
               dispatch_async(dispatch_get_main_queue(), ^{
+//                  NSLog(@"%@",self.timeArray);
+//                  NSLog(@"%@",self.weekArray);
+                  for (NSString *str in self.subjectArray) {
+                      NSLog(@"%@",str);
+                  }
                   
               });
               
