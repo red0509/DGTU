57d25b1614a663c49a3e492854dc6f1e181bffab
red0509 <paa_pavlov@mail.ru>
2016-01-22 13:11:46 +0300

    1.051

--- ViewControllerPageView.m
+++ ViewControllerPageView.m
@@ -13,6 +13,8 @@
 @property (strong,nonatomic) NSMutableArray *timeArray;
 @property (strong,nonatomic) NSMutableArray *weekArray;
 @property (strong,nonatomic) NSMutableArray *subjectArray;
+@property (strong,nonatomic) NSMutableArray *teacherArray;
+@property (strong,nonatomic) NSMutableArray *classroomArray;
 
 @end
 
@@ -32,14 +34,13 @@
     NSArray *viewControllers = @[startingViewController];
     [self.pageViewController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
     
-    self.pageViewController.view.frame = CGRectMake(0, 60, self.view.frame.size.width, self.view.frame.size.height - 30);
+    self.pageViewController.view.frame = CGRectMake(0, 60, self.view.frame.size.width, self.view.frame.size.height - 90);
     
     [self addChildViewController:self.pageViewController];
     [self.view addSubview:self.pageViewController.view];
     [self.pageViewController didMoveToParentViewController:self];
     
     
-     [self loadGroupReference:@"http://stud.sssu.ru/Rasp/Rasp.aspx?group=18856&sem=1"];
     
 }
 
@@ -107,6 +108,8 @@
     self.timeArray = [NSMutableArray array];
     self.weekArray = [NSMutableArray array];
     self.subjectArray = [NSMutableArray array];
+    self.teacherArray = [NSMutableArray array];
+    self.classroomArray = [NSMutableArray array];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSURL *URL = [NSURL URLWithString:URLGroup];
         NSURLSession *session = [NSURLSession sharedSession];
@@ -121,9 +124,10 @@
                                                 contentTypeHeader:contentType];
               NSInteger dayNum = 2;
               NSNumber *dayRow;
+              HTMLElement *day;
               // День недели
               while (YES) {
-              HTMLElement *day = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(1)",(long)dayNum]];
+              day = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(1)",(long)dayNum]];
                   if ([day.textContent isEqualToString:@"Понедельник"]) {
                       dayRow = day.attributes.allValues.lastObject;
                       break;
@@ -134,81 +138,110 @@
               }
               NSInteger dayRowInt = [dayRow integerValue];
               
-              NSInteger timeNum = dayNum;
+              NSInteger section = dayNum;
               NSInteger timeRow;
-              //Время
-              for (; timeNum < dayRowInt+dayNum; timeNum++) {
+              NSInteger weekRow;
+              NSInteger subjectRow;
+              NSInteger teacherRow;
+              NSInteger classroomRow;
+//              Время
+              for (; section < dayRowInt+dayNum; section++) {
                   
-                  if (timeNum == dayNum) {
+                  if (section == dayNum) {
                       timeRow = 2;
+                      weekRow = 3;
+                      subjectRow = 4;
+                      teacherRow = 5;
+                      classroomRow = 6;
                   }else{
                       timeRow = 1;
+                      weekRow = 2;
+                      subjectRow = 3;
+                      teacherRow = 4;
+                      classroomRow = 5;
                   }
                   
-              HTMLElement *time = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)timeNum,(long)timeRow]];
-                  if ([time.textContent isEqualToString:@"2"]) {
-                      [self.timeArray addObject:[self.timeArray objectAtIndex:[self.timeArray count]-1]];
-                  }else{
+              HTMLElement *time = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)timeRow]];
+              HTMLElement *week = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)weekRow]];
+              HTMLElement *subject = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)subjectRow]];
+              HTMLElement *teacher = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)teacherRow]];
+              HTMLElement *classroom = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)classroomRow]];
+                  if ([week.attributes.allValues.lastObject isEqual:@"2"]) {
                       [self.timeArray addObject:time.textContent];
+                      [self.timeArray addObject:time.textContent];
+                      
+                      [self.weekArray addObject:@"1"];
+                      [self.weekArray addObject:@"2"];
+                      subject = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)subjectRow-1]];
+                      [self.subjectArray addObject:subject.textContent];
+                      [self.subjectArray addObject:subject.textContent];
+                      NSLog(@"subject0-   %@",subject.textContent);
+                      
+                      teacher = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)teacherRow-1]];
+                      [self.teacherArray addObject:teacher.textContent];
+                      [self.teacherArray addObject:teacher.textContent];
+                      NSLog(@"teacher0-   %@",teacher.textContent);
+                      
+                      classroom = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)classroomRow-1]];
+                      [self.classroomArray addObject:classroom.textContent];
+                      [self.classroomArray addObject:classroom.textContent];
+                      NSLog(@"classroom0- %@",classroom.textContent);
 
+                  }else{
+                  if ([time.textContent isEqualToString:@"2"]) {
+                      
+                      [self.timeArray addObject:[self.timeArray objectAtIndex:[self.timeArray count]-1]];
+                      NSLog(@"time1-      %@",time.textContent);
+                      
+                      [self.weekArray addObject:@"2"];
+                      NSLog(@"week1-      %@",week.textContent);
+                      
+                    subject = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)subjectRow-1]];
+                      [self.subjectArray addObject:subject.textContent];
+                      NSLog(@"subject1-   %@",subject.textContent);
+                      
+                    teacher = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)teacherRow-1]];
+                      [self.teacherArray addObject:teacher.textContent];
+                       NSLog(@"teacher1-   %@",teacher.textContent);
+                      
+                    classroom = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)section,(long)classroomRow-1]];
+                      [self.classroomArray addObject:classroom.textContent];
+                       NSLog(@"classroom1- %@",classroom.textContent);
+                      
+                  }else{
+                      
+                      [self.timeArray addObject:time.textContent];
+                      NSLog(@"time2-      %@",time.textContent);
+                      
+                      [self.weekArray addObject:week.textContent];
+                      NSLog(@"week2-      %@",week.textContent);
+                      
+                      [self.subjectArray addObject:subject.textContent];
+                      NSLog(@"subject2-   %@",subject.textContent);
+                      
+                      [self.teacherArray addObject:teacher.textContent];
+                      NSLog(@"teacher2-   %@",teacher.textContent);
+                      
+                      [self.classroomArray addObject:classroom.textContent];
+                      NSLog(@"classroom2- %@",classroom.textContent);
                   }
                 }
-              NSInteger weekNum = dayNum;
-              NSInteger weekRow;
-              // номер Неделя
-              for (; weekNum < dayRowInt+dayNum; weekNum++) {
-                  
-                  if (weekNum == dayNum) {
-                      weekRow = 3;
-                  }else{
-                      weekRow = 2;
-                  }
-                  
-                  HTMLElement *week = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)weekNum,(long)weekRow]];
-                  if (![week.textContent isEqualToString:@"1"]) {
-                      [self.weekArray addObject:@"2"];
-                      [self.weekArray addObject:@"Привет"];
-                  }else{
-                      [self.weekArray addObject:week.textContent];
-                      
-                  }
-              }
+            }
               
-              NSInteger subjectNum = dayNum;
-              NSInteger subjectRow;
-              //Дисциплина
-              
-              for (; subjectNum < dayRowInt+dayNum; subjectNum++) {
-                  
-                  NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"SELF contains[c] ст."];
-                  if (subjectNum == dayNum) {
-                      subjectRow = 4;
-                  }else{
-                      subjectRow = 3;
-                  }
-                  
-                  HTMLElement *subject = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)subjectNum,(long)subjectRow]];
-                  if ([subject.textContent isEqualToString:@"ст."]) {
-                      subjectRow--;
-                      HTMLElement *subject = [home firstNodeMatchingSelector:[NSString stringWithFormat:@"#tblGr > tbody > tr:nth-child(%ld) > td:nth-child(%ld)",(long)subjectNum,(long)subjectRow]];
-                      [self.subjectArray addObject:subject.textContent];
-                      subjectRow++;
-                  }else{
-                      [self.subjectArray addObject:subject.textContent];
-
-                  }
-                  
-              }
-
               
            //              NSLog(@"div -  %@",day.attributes.allValues);
               dispatch_async(dispatch_get_main_queue(), ^{
-//                  NSLog(@"%@",self.timeArray);
-//                  NSLog(@"%@",self.weekArray);
+                  NSLog(@"%@",self.timeArray);
+                  NSLog(@"%@",self.weekArray);
                   for (NSString *str in self.subjectArray) {
                       NSLog(@"%@",str);
                   }
-                  
+                  for (NSString *str in self.teacherArray) {
+                      NSLog(@"%@",str);
+                  }
+                  for (NSString *str in self.classroomArray) {
+                      NSLog(@"%@",str);
+                  }
               });
               
           }
